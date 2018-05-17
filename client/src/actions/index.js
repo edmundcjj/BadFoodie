91b4b47e302bbd5fa0ad8c49bb03e753bce97d88
Edmund <edmundcjj@gmail.com>
2018-05-18 03:56:59 +0800

    Added actions for the reviews CRUD

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+/*====================REVIEWS================================*/
 export function getReviews(limit = 10, start = 0, order = 'asc', list = ''){
   const request = axios.get(`/api/reviews?limit=${limit}&skip=${start}&order=${order}`)
                   .then(response => {
@@ -49,4 +50,95 @@ export function clearReviewWithReviewer(){
             reviewer:{}
         }
     }
+}
+
+export function addReview(review){
+    console.log(review)
+    const request = axios.post('/api/review',review)
+        .then(response => response.data);
+
+    return {
+        type:'ADD_REVIEW',
+        payload:request
+    }
+}
+
+export function clearNewReview() {
+    return {
+        type:'CLEAR_NEWREVIEW',
+        payload:{}
+    }
+}
+
+export function getUserPosts(userId){
+    const request = axios.get(`/api/user_posts?user=${userId}`)
+                    .then(response => response.data);
+
+    return {
+        type:'GET_USER_POSTS',
+        payload:request
+    }
+}
+
+export function getReview(reviewId){
+    const request = axios.get(`/api/getReview?id=${reviewId}`)
+                    .then(response => response.data);
+
+    return {
+        type:'GET_REVIEW',
+        payload:request
+    }
+}
+
+export function updateReview(data){
+    const request = axios.post(`/api/update_review`, data)
+                    .then(response => response.data);
+
+    return {
+        type:'UPDATE_REVIEW',
+        payload:request
+    }
+}
+
+export function deleteReview(reviewId){
+    const request = axios.delete(`/api/delete_review?id=${reviewId}`)
+                    .then(response => response.data);
+
+    return {
+        type:'DELETE_REVIEW',
+        payload:request
+    }
+}
+
+export function clearReview(){
+    return {
+        type:'CLEAR_REVIEW',
+        payload:{
+          review:null,
+          updateReview: false,
+          postDeleted: false
+        }
+    }
+}
+
+/*====================USER================================*/
+export function loginUser({email,password}){
+    const request = axios.post('/api/login',{email,password})
+                .then(response => response.data);
+
+    return {
+        type:'USER_LOGIN',
+        payload:request
+    }
+}
+
+export function auth(){
+    const request = axios.get('/api/auth')
+                .then(response => response.data);
+
+    return {
+        type:'USER_AUTH',
+        payload:request
+    }
+
 }
