30a38b564d9d2bafd3d1c7f39537e8e9bdd47c8a
Edmund <edmundcjj@gmail.com>
2018-05-17 17:23:54 +0800

    Container to hold all the reviews

---
+++ client/src/containers/home_container.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getReviews } from './../actions';
+import ReviewItem from './../widgetsUI/review_item';
+
+class HomeContainer extends Component {
+
+  constructor() {
+    super()
+
+    this.renderItems = this.renderItems.bind(this);
+    this.loadmore = this.loadmore.bind(this);
+  }
+
+  componentWillMount() {
+    this.props.dispatch(getReviews(2, 0, 'asc'))
+  }
+
+  renderItems = (reviews) => (
+    reviews.list ?
+      reviews.list.map( item => (
+        <ReviewItem {...item} key={item._id} />
+      ))
+    :null
+  )
+
+  loadmore = () => {
+    let currentCount = this.props.reviews.list.length;
+
+    this.props.dispatch(getReviews(2, currentCount, 'asc', this.props.reviews.list))
+  }
+
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        {this.renderItems(this.props.reviews)}
+        <div className="loadmore" onClick={this.loadmore}>LOAD MORE</div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    reviews: state.reviews
+  }
+}
+
+export default connect(mapStateToProps)(HomeContainer);
