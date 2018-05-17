c248696f87c51a56aa10c1b17d33274ffcc75154
Edmund <edmundcjj@gmail.com>
2018-05-18 04:04:30 +0800

    Added more reducers for reviews

--- client/src/reducers/reviews_reducer.js
+++ client/src/reducers/reviews_reducer.js
@@ -4,6 +4,9 @@ export default function(state={}, action){
     case 'GET_REVIEWS':
       return { ...state, list:action.payload }
 
+    case 'GET_REVIEW':
+      return { ...state, review:action.payload }
+
     case 'GET_REVIEW_W_REVIEWER':
       return {
         ...state,
@@ -16,6 +19,30 @@ export default function(state={}, action){
         ...state,
         review:action.payload.review,
         reviewer: action.payload.reviewer
+      }
+
+    case 'ADD_REVIEW':
+      return { ...state, newreview:action.payload }
+
+    case 'CLEAR_NEWREVIEW':
+      return {...state, newreview:action.payload}
+
+    case 'UPDATE_REVIEW':
+      return {
+        ...state,
+        updatereview:action.payload.success,
+        review: action.payload.doc
+      }
+
+    case 'DELETE_REVIEW':
+      return {...state, postDeleted:action.payload}
+
+    case 'CLEAR_REVIEW':
+      return {
+        ...state,
+        updatereview:action.payload.updateReview,
+        review: action.payload.review,
+        postDeleted: action.payload.postDeleted
       }
 
     default:
